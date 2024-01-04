4cc2bf3dfee042755974bf0f36a4edf6b263fcc0
giaan7904 <150408222+giaan7904@users.noreply.github.com>
2024-01-04 07:35:21 +0700

    commit
    commit

--- untitled1/main.c
+++ untitled1/main.c
@@ -1,43 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-void chuanHoa(char s[]) {
-    int i = 0, j = 0;
-    while (s[i] == ' ') i++;
-    while (s[i] != '\0') {
-        if (s[i] == ' ' && s[i + 1] == ' ') {
-            i++;
-            continue;
-        }
-        s[j++] = s[i++];
-    }
-    s[j] = '\0';
+struct Student {
+    char name[50];
+    char class[20];
+    char dob[15];
+    float gpa;
+};
 
-    for (i = 0; i < strlen(s); i++) {
-        if (i == 0 || s[i - 1] == ' ') s[i] = toupper(s[i]);
-        else s[i] = tolower(s[i]);
-    }
-}
+int compare(const void *a, const void *b) {return (*(struct Student*)b).gpa - (*(struct Student*)a).gpa;}
 
 int main() {
-    FILE *f1, *f2;
-    f1 = fopen("sinhvien.txt", "r");
-    f2 = fopen("sinhvien_out.txt", "w");
+    FILE *inputFile, *outputFile;
+    struct Student students[100];
+    int count = 0;
 
-    int n;
-    fscanf(f1, "%d\n", &n);
-    fprintf(f2, "%d\n", n);
 
-    for (int i = 0; i < n; i++) {
-        char s[100];
-        fgets(s, sizeof(s), f1);
-        chuanHoa(s);
-        fputs(s, f2);
+    inputFile = fopen("sv_in.txt", "r");
+    if (inputFile == NULL) {
+        printf("Error opening file");
+        return 1;
     }
 
-    fclose(f1);
-    fclose(f2);
+    char line[100];
+    while (fgets(line, sizeof(line), inputFile)) {
+        sscanf(line, "%[^,],%[^,],%[^,],%f", students[count].name, students[count].class, students[count].dob,&students[count].gpa);
+        count++;
+    }
+
+    qsort(students, count, sizeof(struct Student), compare);
+
+    outputFile = fopen("sv_out.txt", "w");
+    if (outputFile == NULL) {
+        printf("Error opening file");
+        return 1;
+    }
+
+
+    for (int i = 0; i < count; i++) {
+        fprintf(outputFile, "%s,%s,%s,%.2f", students[i].name, students[i].class, students[i].dob, students[i].gpa);
+    }
+
+    fclose(inputFile);
+    fclose(outputFile);
 
     return 0;
 }
